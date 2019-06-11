ee40c26064a550223fb4e4211c1d6c88b4375917
patrykseweryn <patryk.seweryn@gmail.com>
2019-06-11 23:13:04 +0200

    small change

--- chapter_8/files.py
+++ chapter_8/files.py
@@ -1,5 +1,7 @@
 import os
 
+
+
 list_of_files = os.listdir('/usr/')
 absolute_path = '/Users/patrykseweryn/PycharmProjects/stupski_tutorials/chapter_8/files.py'
 relative_path = 'chapter_8/files.py'
